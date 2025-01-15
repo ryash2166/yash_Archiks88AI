56f8033a273746b3bf5cea2d43684c6c59e0e954
Yash Rakholiya <sqyash786@gmail.com>
2025-01-15 13:18:17 +0530

    make a conditionally render for Ai Video and Ai Image page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import Sidebar from "../Components/Sidebar/Sidebar";
-import { Outlet, useLocation } from "react-router";
+import { Outlet, useLocation } from "react-router-dom";
 
 const Router = () => {
+  const location = useLocation();
+
+  const shouldShowSidebar = !["/ImageAI", "/VideoAI"].includes(location.pathname);
+
   return (
     <>
       <Navbar />
-      {/* <Sidebar /> */}
+      {shouldShowSidebar && <Sidebar />}
       <Outlet />
     </>
   );
