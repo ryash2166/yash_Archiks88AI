05470744535324f747b075c7140c6c91e45f6793
Yash Rakholiya <sqyash786@gmail.com>
2025-01-20 14:59:02 +0530

    footer updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import logo from "../../assets/logo.svg";
 import { FaAngleRight } from "react-icons/fa";
-import QRDiscord from "../../assets/QRDiscord.png";
-import QRYoutube from "../../assets/QRYoutube.png";
-import QRTwitter from "../../assets/QRTwitter.png";
+import discord from "../../assets/discord.svg";
+import youtube from "../../assets/youtube.svg";
+import twitter from "../../assets/twitter.svg";
 
 const Footer = () => {
   return (
     <div className="pt-10 pb-9 px-6 lg:ml-[260px] lg:pr-[42px] border-t-[1px] border-[#333]">
       <div className="text-white flex flex-wrap justify-between gap-3">
-        <div className="mb-6 lg:mb-0 w-full lg:w-auto flex items-start  justify-center ">
+        <div className="mb-6 lg:mb-0 w-full lg:w-auto flex items-start justify-center ">
           <img src={logo} alt="Logo" className="" />
         </div>
 
@@ -40,34 +40,16 @@ const Footer = () => {
         </div>
 
         <div className="block lg:flex justify-center lg:justify-end w-full lg:w-auto lg:space-x-6">
-          <div className="text-center max-lg:mb-3">
-            <img
-              src={QRDiscord}
-              alt="Discord QR"
-              width={112}
-              height={112}
-              className="rounded-lg mb-2 mx-auto"
-            />
-            <p>Discord</p>
+          <div className="flex flex-col items-center max-lg:mb-4">
+            <img src={discord} alt="" width={80} height={80} className="mb-1" />
+            <p className="text-center">Discord</p>
           </div>
-          <div className="text-center max-lg:mb-3">
-            <img
-              src={QRYoutube}
-              alt="YouTube QR"
-              width={112}
-              height={112}
-              className="rounded-lg mb-2 mx-auto"
-            />
+          <div className="flex flex-col items-center max-lg:mb-4">
+            <img src={youtube} alt="" width={80} height={80} className="mb-1" />
             <p>YouTube</p>
           </div>
-          <div className="text-center">
-            <img
-              src={QRTwitter}
-              alt="Twitter QR"
-              width={112}
-              height={112}
-              className="rounded-lg mb-2 mx-auto"
-            />
+          <div className="flex flex-col items-center">
+            <img src={twitter} alt="" width={80} height={80} className="mb-1" />
             <p>Twitter</p>
           </div>
         </div>
@@ -75,9 +57,7 @@ const Footer = () => {
 
       {/* Footer Bottom Section */}
       <div className="text-xs text-center mt-12 mb-20 text-[#4e495a]">
-        <p>
-          Archiks88ai.com 2024 © All rights Reserved.
-        </p>
+        <p>Archiks88ai.com 2024 © All rights Reserved</p>
       </div>
     </div>
   );
